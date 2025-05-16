7cc5103ec81d913494080906394ca4e7a9f64117
Ed Groell <email@edgroell.com>
2025-05-16 16:48:09 +0200

    new increment x1 - (1) restructured project folder to follow best practices

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,8 +1,13 @@
 import json
+import os
 
 
 def load_data(file_path: str) -> list:
-    """ Loads a JSON file """
+    """
+    Loads data from json file.
+    :param file_path:
+    :return:
+    """
     with open(file_path, "r") as handle:
         all_data = json.load(handle)
         return all_data
@@ -18,45 +23,52 @@ def serialize_animal(animal):
     animals_cards += ('<div class="card__title">'
                       + animal.get('name', 'Name not found')
                       + '</div>\n')
-    animals_cards += '<p class="card__text">'
-    animals_cards += ('<strong>'
+    animals_cards += '<div class="card__text">'
+    animals_cards += '<ul>\n'
+    animals_cards += ('<li>'
+                      + '<strong>'
                       + 'Diet:'
                       + '</strong>'
                       + ' '
                       + animal.get('characteristics', {}).get('diet', 'Diet not found')
-                      + '<br/>\n')
+                      + '</li>\n')
 
     if animal.get('characteristics', {}).get('type') is not None:
-        animals_cards += ('<strong>'
+        animals_cards += ('<li>'
+                          + '<strong>'
                           + 'Type:'
                           + '</strong>'
                           + ' '
                           + animal.get('characteristics', {}).get('type').capitalize()
-                          + '<br/>\n')
+                          + '</li>\n')
 
-    animals_cards += ('<strong>'
+    animals_cards += ('<li>'
+                      + '<strong>'
                       + 'Location(s):'
                       + '</strong>'
                       + ' '
                       + f"{locations_string}"
-                      + '<br/>\n')
-    animals_cards += ('<strong>'
+                      + '</li>\n')
+    animals_cards += ('<li>'
+                      + '<strong>'
                       + 'Lifespan:'
                       + '</strong>'
                       + ' '
                       + animal.get('characteristics', {}).get('lifespan', 'Lifespan not found')
-                      + '<br/>\n')
+                      + '</li>\n')
 
     if animal.get('characteristics', {}).get('top_speed') is not None:
-        animals_cards += ('<strong>'
+        animals_cards += ('<li>'
+                          + '<strong>'
                           + 'Top Speed:'
                           + '</strong>'
                           + ' '
                           + animal.get('characteristics', {}).get('top_speed')
-                          + '<br/>\n')
+                          + '</li>\n')
 
-    animals_cards += '</p>\n'
-    animals_cards += f"</li>\n"
+    animals_cards += '</ul>\n'
+    animals_cards += '</div>\n'
+    animals_cards += '</li>\n'
 
     return animals_cards
 
@@ -85,14 +97,17 @@ def inject_animal_cards(page_template: str, animal_cards: str) -> str:
 
 def build_repository_page(page: str) -> None:
     """ Builds the repository page """
-    with open("animals.html", "w") as handle:
+    if not os.path.exists('output'):
+        os.mkdir('output')
+    file_path = os.path.join('output', 'animals.html')
+    with open(file_path, "w") as handle:
         handle.write(page)
 
 
 def main():
-    animals_data = load_data('animals_data.json')
+    animals_data = load_data('data/animals_data.json')
     animals_cards = get_animal_cards(animals_data)
-    page_template = open_template('animals_template.html')
+    page_template = open_template('templates/animals_template.html')
     new_page_template = inject_animal_cards(page_template, animals_cards)
     build_repository_page(new_page_template)
 
